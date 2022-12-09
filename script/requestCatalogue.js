9de7c2f1d813b168d0c423fb63554f306446f160
Agil Adi Saputro <agiladisaputro@gmail.com>
2022-12-09 07:00:35 +0700

    add create, delete katalog without images file

--- script/requestCatalogue.js
+++ script/requestCatalogue.js
@@ -15,12 +15,51 @@ const getCatalogue = async () => {
 };
 
 // Create
+const insertCatalogue = async (catalogue) => {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(catalogue)
+    };
+    console.log(catalogue);
+    
+    const response = await fetch(`${baseUrl}`, options)
+    const responseJson = await response.json();
+    if (responseJson.error) {
+      showResponseMessage(responseJson.message);
+    } else {
+      // renderAllBooks(responseJson.books);
+      getCatalogue();
+    }
+  } catch (error) {
+    showResponseMessage(error)
+  }
+};
 
+// Delete
+const removeCatalogue = (catalogueId) => {
+  fetch(`${baseUrl}/${catalogueId}`, {
+    method: 'DELETE',
+  })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseJson => {
+        // showResponseMessage(responseJson.message);
+        getCatalogue();
+      })
+      .catch(error => {
+        showResponseMessage(error);
+      });
+};
 
 // Render
 const renderListCatalogue = (listsCatalogue) => {
-  const listCataloguelement = document.querySelector('#listBook');
-  listCataloguelement.innerHTML = '';
+  const listCatalogueElement = document.querySelector('#listCatalogue');
+  listCatalogueElement.innerHTML = '';
 
   let number = 1;
   listsCatalogue.forEach(catalogue => {
@@ -32,7 +71,7 @@ const renderListCatalogue = (listsCatalogue) => {
 
     const dateString = currentDayOfMonth + "-" + (currentMonth + 1) + "-" + currentYear;
 
-    listCataloguelement.innerHTML += `
+    listCatalogueElement.innerHTML += `
       <tr>
         <td class="text-center">${number++}</td>
         <td>${catalogue.type}</td>
@@ -43,33 +82,59 @@ const renderListCatalogue = (listsCatalogue) => {
           <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modal-edit">
             <i class="fas fa-pencil-alt"></i> Edit
           </button>
-          <a class="btn btn-danger btn-sm" href="#">
+          <button type="button" id=${catalogue.id} class="button-delete btn btn-danger btn-sm">
             <i class="fas fa-trash"> </i>
             Delete
-          </a>
+          </button>
         </td>
       </tr>
     `;
   });
 
-  const showResponseMessage = (message = 'Check your internet connection') => {
-    alert(message);
-  };
+  // Delete
+  const buttonUpdate = document.querySelector('#buttonUpdate');
+  const buttonDelete = document.querySelectorAll('.button-delete');
 
-  // document.addEventListener('DOMContentLoaded', () => {
-  //   const  inputBookId
-  //   const buttonUpdate = document.querySelector('#buttonUpdate');
-
-  //   buttonSave.addEventListener('click', () => {
-  //     const catalogue = {
-  //       id: 
-  //       type
-  //       name
-  //       price
-  //       satuan
-  //     }
-  //   });
-  // });
-
+  buttonDelete.forEach(button => {
+    button.addEventListener('click', event => {
+      event.preventDefault();
+      if (confirm('Are you sure you want to delete these item?')) {
+        const catalogueId = event.target.id;
+        console.log("hapus katalog id:" + catalogueId);
+        removeCatalogue(catalogueId);
+      }
+    }, false);
+  });
 };
+
+const showResponseMessage = (message = 'Check your internet connection') => {
+  alert(message);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const inputName = document.querySelector('#inputName');
+  const inputType = document.querySelector('#inputType');
+  const inputPrice = document.querySelector('#inputPrice');
+  const inputUnit = document.querySelector('#inputUnit');
+  const inputCatalogueCode = document.querySelector('#inputCatalogueCode');
+  const inputImagePath = document.querySelector('#inputImagePath');
+
+  const buttonSave = document.querySelector('#buttonSave');
+
+  buttonSave.addEventListener('click', () => {
+    const catalogue = {
+      name: inputName.value,
+      type: inputType.options[inputType.selectedIndex].value,
+      description: "belum kepake",
+      price: parseInt(inputPrice.value),
+      satuan: inputUnit.options[inputUnit.selectedIndex].value,
+      kode_katalog: inputCatalogueCode.value,
+      image: inputImagePath.value
+    };
+    console.log(catalogue);
+    insertCatalogue(catalogue);
+  });
+
+});
+
 getCatalogue();
